4cc5643ce830b8506dc22ce3e3c7809b9a8f99c5
marinaP <mar.pavlovic@gmail.com>
2021-11-29 21:43:57 +0100

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+  onSearchSubmit(term) {
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '15px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App;
